c1c40432c11b139f428a5e7f385e201361b7bac8
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2021-07-13 20:41:58 +0900

    Add files via upload

---
+++ p144.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace p144_ex1
+{
+    class p144
+    {
+        static void Main(string[] args)
+        {
+            // 1. i++와 ++i의 차이점은 무엇인가요?
+            //  답 : 
+            //
+            // 2. 각 출력문의 값을 적으세요
+            //      int i = 1;
+            //      Console.WriteLine(i = i + 1);   답 :
+            //      Console.WriteLine(i++);         답 :
+            //      Console.WriteLine(++i);         답 :
+            //      Console.WriteLine(i += 1);      답 :
+            //
+            // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : 
+            //      int a = 10;
+            //      string b = a == 0 ? "가나다" : "ABC";
+        }
+    }
+}
