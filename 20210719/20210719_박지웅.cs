652543dc48368c2417e3a2798d5537f31a7f6dca
MudskippUh <86944319+MudskippUh@users.noreply.github.com>
2021-07-19 16:39:59 +0900

    Update 20210719_박지웅.cs

--- 20210719/20210719_박지웅.cs
+++ 20210719/20210719_박지웅.cs
@@ -8,14 +8,16 @@ namespace p144_ex1
         static void Main(string[] args)
         {
             // 1. i++와 ++i의 차이점은 무엇인가요?
-            //  답 : 전위증가연산자, 후휘증가연산자. 연산자를 변수 뒤에 사용하면 해당 문장의 실행이 끝난 후에 변수의 값이 변경되고, 변수 앞에 사용하면 변수의 값을 변경한 우에 해당문장이 실행된다.
+            //  답 : 전위증가연산자, 후휘증가연산자. 
+            //  연산자를 변수 뒤에 사용하면 해당 문장의 실행이 끝난 후에 변수의 값이 변경되고,
+            //  변수 앞에 사용하면 변수의 값을 변경한 우에 해당 문장이 실행된다.
             //
             // 2. 각 출력문의 값을 적으세요
             //      int i = 1;
             //      Console.WriteLine(i = i + 1);   답 :2
             //      Console.WriteLine(i++);         답 :2
-            //      Console.WriteLine(++i);         답 :3
-            //      Console.WriteLine(i += 1);      답 :2
+            //      Console.WriteLine(++i);         답 :4
+            //      Console.WriteLine(i += 1);      답 :5
             //
             // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : ABC
             //      int a = 10;
