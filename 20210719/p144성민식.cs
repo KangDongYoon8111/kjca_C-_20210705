169fea6fef73355c5f194c12cc66cc5e9e96cec7
mssung34 <86944313+mssung34@users.noreply.github.com>
2021-07-19 16:26:59 +0900

    Add files via upload

---
+++ 20210719/p144성민식.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P144_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. i++와 ++i의 차이점은 무엇인가요?
+            //  답 : i++는 작업수행 후 +1 ++i는 +1후 작업수행
+            //
+            // 2. 각 출력문의 값을 적으세요
+            //      int i = 1;
+            //      Console.WriteLine(i = i + 1);   답 :2
+            //      Console.WriteLine(i++);         답 :2
+            //      Console.WriteLine(++i);         답 :4
+            //      Console.WriteLine(i += 1);      답 :5
+            //
+            // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : ABC
+            //      int a = 10;
+            //      string b = a == 0 ? "가나다" : "ABC";
+        }
+    }
+}
