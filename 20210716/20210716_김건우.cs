88dd2b1132a1efd0fb0aaee47812f0ff36029c7f
강동윤 <46252404+KangDongYoon8111@users.noreply.github.com>
2021-07-19 13:49:17 +0900

    Update 20210716_김건우.cs

--- 20210716/20210716_김건우.cs
+++ 20210716/20210716_김건우.cs
@@ -80,20 +80,6 @@ namespace ConsoleApp3
             int c10 = Convert.ToInt32(Console.ReadLine());
             int d10 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"출력 : {a10}, {b10}, {c10}, {d10}");
-
-            // 1. i++와 ++i의 차이점은 무엇인가요?
-            //  답 : 선 연산 후 증가 , 선 증가 후 연산의 차이 (연산자 우선순위)
-            //
-            // 2. 각 출력문의 값을 적으세요
-            //      int i = 1;
-            //      Console.WriteLine(i = i + 1);   답 : 2
-            //      Console.WriteLine(i++);         답 : 2 (선 연산 후 증가)
-            //      Console.WriteLine(++i);         답 : 4 (선 증가 후 연산)
-            //      Console.WriteLine(i += 1);      답 : 5
-            //
-            // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : "ABC" (삼항 조건 연산자 사용법)
-            //      int a = 10;
-            //      string b = a == 0 ? "가나다" : "ABC";
         }
     }
 }
