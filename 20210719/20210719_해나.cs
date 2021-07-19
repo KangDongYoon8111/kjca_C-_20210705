be7f78a0f49e9eeee64611128315f9f9b0a6fcb3
eveyuma <86944291+eveyuma@users.noreply.github.com>
2021-07-19 16:27:23 +0900

    Add files via upload

---
+++ 20210719/20210719_해나.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace p144_ex1
+{
+    class p144
+    {
+        static void Main(string[] args)
+        {
+            // 1. i++와 ++i의 차이점은 무엇인가요?
+            //  답 : i++은 i에 입력된 데이터를 배출한 뒤 1을 더함 i+1
+            //++i 는 i의 데이터에 1을 더한 값을 배출함
+            //
+            // 2. 각 출력문의 값을 적으세요
+            //      int i = 1; 
+            //      Console.WriteLine(i = i + 1);   답 : 2
+            //      Console.WriteLine(i++);         답 : 2
+            //      Console.WriteLine(++i);         답 : 3
+            //      Console.WriteLine(i += 1);      답 : 5
+            //
+            // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : ABC
+            //      int a = 10;
+            //      string b = a == 0 ? "가나다" : "ABC";
+        }
+    }
+}
