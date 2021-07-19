a40606436d10626b8e56706c25f319c5e99d5719
MudskippUh <86944319+MudskippUh@users.noreply.github.com>
2021-07-19 16:31:14 +0900

    Add files via upload

---
+++ 20210719/20210719_박지웅.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace p144_ex1
+{
+    class p144
+    {
+        static void Main(string[] args)
+        {
+            // 1. i++와 ++i의 차이점은 무엇인가요?
+            //  답 : 출력했을 때 값이 i++는 1이 더해지고 ++i는 2가 더해진다.
+            //
+            // 2. 각 출력문의 값을 적으세요
+            //      int i = 1;
+            //      Console.WriteLine(i = i + 1);   답 :2
+            //      Console.WriteLine(i++);         답 :2
+            //      Console.WriteLine(++i);         답 :3
+            //      Console.WriteLine(i += 1);      답 :2
+            //
+            // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : ABC
+            //      int a = 10;
+            //      string b = a == 0 ? "가나다" : "ABC";
+        }
+    }
+}
