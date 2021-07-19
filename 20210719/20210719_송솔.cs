a8e1fcc85f4539888a31f3a43d25d2a07417acf0
likemilktea <86944264+likemilktea@users.noreply.github.com>
2021-07-19 16:32:28 +0900

    Update 20210719_송솔.cs

--- 20210719/20210719_송솔.cs
+++ 20210719/20210719_송솔.cs
@@ -7,13 +7,13 @@ namespace p144_ex1
         static void Main(string[] args)
         {
             // 1. i++와 ++i의 차이점은 무엇인가요?
-            //  답 : i++ 출력 전 +1, ++i 출력 후 +1
+            //  답 : i++ 출력 후 +1, ++i 출력 전 +1
             //
             // 2. 각 출력문의 값을 적으세요
             //      int i = 1;
             //      Console.WriteLine(i = i + 1);   답 :2
-            //      Console.WriteLine(i++);         답 :3
-            //      Console.WriteLine(++i);         답 :3
+            //      Console.WriteLine(i++);         답 :2
+            //      Console.WriteLine(++i);         답 :4
             //      Console.WriteLine(i += 1);      답 :5
             //
             // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : ABC
