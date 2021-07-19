83490e23ca53b9df5e6eccbf21233f7dfaf608eb
myooto <86944250+myooto@users.noreply.github.com>
2021-07-19 16:29:15 +0900

    Add files via upload

---
+++ 20210719/20210719_김도현.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace p144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. i++와 ++i의 차이점은 무엇인가요?
+            //  답 : i++는 후위증가연산자로 출력 후에 결과 값이 증가, ++i는 전위 증가 연산자로 결과 값이 증가 후 출력
+            //
+            // 2. 각 출력문의 값을 적으세요
+            //      int i = 1;
+            //      Console.WriteLine(i = i + 1);   답 : 2
+            //      Console.WriteLine(i++);         답 : 2
+            //      Console.WriteLine(++i);         답 : 4
+            //      Console.WriteLine(i += 1);      답 : 5
+
+            //
+            // 3. 다음 코드에서 b는 어떤 값을 가질까요?    답 : ABC
+            //      int a = 10;
+            //      string b = a == 0 ? "가나다" : "ABC";
+
+        }
+    }
+}
