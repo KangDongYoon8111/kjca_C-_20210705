eabdacca1bc94d759e7b002f0405536a70fbd504
https://gitlab.com/codedmouse/config_modem.git <codedmouse@gmail.com>
2021-07-23 16:29:39 +0900

    반복문을 이용하지 않은 반복 2가지

---
+++ 20210723/20210723_김건우.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+	class MainApp
+	{
+		static void LoopTest(int i)
+        {
+			if (i > 119) { return; }
+			Console.WriteLine(i);
+			LoopTest(++i);
+        }
+
+		static void LoopTest2(int i)
+        {
+			goto Loop;
+		Loop:
+			Console.WriteLine(i++);
+			if (i <= 119) { goto Loop; }
+        }
+
+		static void Main(string[] args)
+		{
+			LoopTest(0);
+			Console.WriteLine();
+
+			LoopTest2(0);
+		}
+	}
+}
