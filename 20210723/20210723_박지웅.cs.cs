b05d90f266c1c67391e2ad981d225d9036f20fcb
MudskippUh <86944319+MudskippUh@users.noreply.github.com>
2021-07-23 16:47:55 +0900

    Add files via upload

---
+++ 20210723/20210723_박지웅.cs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace p169_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("반복 횟수를 입력하세요 : ");
+             String input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+
+                if (number <= 0) Console.WriteLine("O보다 작거나 같은 수는 입력할 수 없습니다.");
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+//책에 나온 것까지만 했습니다.
