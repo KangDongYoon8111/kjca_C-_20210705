d4b2f95eca6a73d55e3f5bb58edcdcc4c1a735f2
mssung34 <86944313+mssung34@users.noreply.github.com>
2021-07-23 16:48:12 +0900

    Add files via upload

---
+++ 20210723/20210723_성민식.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace p182_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool q = true;
+            while (q)
+            {
+                Write("반복횟수를 입력하세요.");
+                int a = Convert.ToInt32(ReadLine());
+
+                if (a <= 0)
+                    WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+                else if (a == 119)
+                    q = false;
+                else
+                {
+                    for (int b = 0; b <= a; b++)
+                    {
+                        for (int c = 0; c < b; c++)
+                        {
+                            Write("*");
+                        }
+                        WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
