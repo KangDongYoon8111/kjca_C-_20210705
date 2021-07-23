982e1f351194928703f4639568437b19c069e78a
ohsunmin <87164935+ohsunmin@users.noreply.github.com>
2021-07-23 16:49:32 +0900

    Add files via upload

---
+++ 20210723/20210723_오선민.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace _20210723_osm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool a = true;
+
+            while (a)
+            {
+                Write("반복 횟수를 입력하세요");
+                int b = Int32.Parse(ReadLine());
+
+                if (b > 0)
+                {
+                    for (int i = 0; i < b; i++)
+                    {
+                        for (int j = 0; j <= i; j++)
+                        {
+                            Write("*");
+                        }
+                        WriteLine();
+                    }
+                }
+                else
+                    WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+                if (b == 119)
+                {
+                    a = false;
+                }
+            }
+        }
+    }
+} 
