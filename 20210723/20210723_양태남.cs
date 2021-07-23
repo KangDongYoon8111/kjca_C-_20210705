84d1129e2dc2be1dec0a02aaaa02e110524288b9
yangtaenam <87164895+yangtaenam@users.noreply.github.com>
2021-07-23 16:46:35 +0900

    Add files via upload

---
+++ 20210723/20210723_양태남.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _20210723_양태남
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("반복 횟수를 입력하세요.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 0)
+            {
+                Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+            }
+            
+            while (number != 119)
+            {
+                for (int a = 0; a < number; a++)
+                {
+                    for (int b = 0; b <= a; b++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
