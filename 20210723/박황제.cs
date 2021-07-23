0cf67adf7f1308af1bfb67f7272482e1a17c1b24
hi2222213 <87515360+hi2222213@users.noreply.github.com>
2021-07-23 16:50:22 +0900

    Add files via upload

---
+++ 20210723/박황제.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _168ex_1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("0보다 같거나 작은 숫자는 사용할수 없습니다.");
+            Console.WriteLine(" 반복 횟수를 입력하세요")
+
+
+
+
+
+            {
+                for (int i = 0; i < 5; i++)
+
+
+
+
+                {
+                    for (int j = 0; j <= i; j++)
+
+
+
+
+
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+        
+                  
+
+
+
+        
+
+        
