00518c8959f63527d9a8dbca481034d91247e853
rt7356 <86944308+rt7356@users.noreply.github.com>
2021-07-28 15:45:56 +0900

    Add files via upload

--- 20210728/20210728_ex_곽재우.cs
+++ 20210728/20210728_ex_곽재우.cs
@@ -1,24 +1,37 @@
 ﻿using System;
 
-namespace _20200728_ex2_곽재우.cs
+namespace _20200728_ex3_곽재우.cs
 {
     class Program
     {
         public static void Main()
         {
-            double mean = 0;
 
-            Mean(1, 2, 3, 4, 5, ref mean);
 
-            Console.WriteLine("평균 : {0}", mean); ;
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+            plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+            Plus(x, y, out resultB); //오버로드가 필요한 메소드입니다.
+
+            Console.WriteLine("{0} + {1} + {2}", x, y, resultB);
         }
 
-        public static void Mean(
-            double a, double b, double c,
-            double d, double e, ref double mean)
+        public static void plus(int a, int b, out int c)
         {
-            mean = (a + b + c + d + e) / 5;
-        
+            c = a + b;
+        }
+        public static void Plus(double a, double b, out double c)
+        {
+            c = a + b;
         }
     }
 }
