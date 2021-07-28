d4804e14807f7d284d75b0668c9bbcf737c12c5e
mssung34 <86944313+mssung34@users.noreply.github.com>
2021-07-28 15:29:43 +0900

    Add files via upload

---
+++ 20210728/20210728_ex2_성민식.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _20210728_ex2_성민식
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, ref mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(
+            double a, double b, double c,
+            double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
