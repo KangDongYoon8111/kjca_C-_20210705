5d07989520799dde1c5640a74c2a23f9109532b7
ohsunmin <87164935+ohsunmin@users.noreply.github.com>
2021-07-28 15:30:18 +0900

    Add files via upload

---
+++ 20210728/20210728_ex1_오선민.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace p216_ex1
+{
+    class Program
+    {
+        static double Square(double a)
+        {
+            return a * a;
+        }
+        
+        static void Main(string[] args)
+        {
+
+            Console.Write("수를 입력하세요. : ");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("결과 : {0}", Square(input));
+
+        }   
+    }
+}
