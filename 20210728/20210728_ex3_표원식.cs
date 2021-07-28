b941a4dcfe199be85811ccb79ae7e45820706f70
mylove9573 <86944269+mylove9573@users.noreply.github.com>
2021-07-28 15:31:21 +0900

    Add files via upload

---
+++ 20210728/20210728_ex3_표원식.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210728_ex3_표원식
+{
+    class Class2
+    {
+
+       /* public static void Main()
+        {
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+            plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+            plus(x, y, out resultB);
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, resultB);
+
+        }
+
+        public static void plus(int a,int b, out int c)
+        {
+            c = a + b;
+        }
+
+        public static void plus(double x, double y, out double z)
+        {
+            z = x + y;
+        }
+*/
+
+
+
+
+
+    }
+}
