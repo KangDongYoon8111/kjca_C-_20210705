41a962941a59d6eb2fcada83a87675f84af1183e
mylove9573 <86944269+mylove9573@users.noreply.github.com>
2021-07-28 15:33:03 +0900

    Add files via upload

---
+++ 20210728/20210728_ex2_표원식.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210728_ex2_표원식
+{
+    class Class1
+    {
+
+       public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, ref mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(
+            double a, double b, double c,
+            double d, double e, ref double mean)
+
+        { 
+            mean = (a + b + c + d + e) / 5; 
+
+            //다 더하면 15 
+        
+        }
+
+
+
+
+
+
+
+
+    }
+}
