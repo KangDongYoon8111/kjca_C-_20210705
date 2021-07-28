432d4182a98b9d8badbd0971b568875df28c627b
likemilktea <86944264+likemilktea@users.noreply.github.com>
2021-07-28 15:29:45 +0900

    Add files via upload

---
+++ 20210728/20210728_ex1_송솔.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p216_ex1
+{
+    class Program
+    {
+        static double Square(double arg)
+        {
+            double i = arg * arg;
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하시오 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
---
+++ 20210728/20210728_ex2_송솔.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace p217_ex1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, out mean);
+
+            Console.WriteLine("평균 : {0}",mean);
+        }
+        public static void Mean(
+            double a,double b,double c,double d,double e,out double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
