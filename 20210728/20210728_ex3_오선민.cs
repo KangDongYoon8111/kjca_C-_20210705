9d2116bc61bb54d541ed6626f57370b29f550bd6
ohsunmin <87164935+ohsunmin@users.noreply.github.com>
2021-07-28 15:30:44 +0900

    Add files via upload

---
+++ 20210728/20210728_ex3_오선민.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace p217_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+            Plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+            Plus(x, y, out resultB);
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, resultB);
+        }
+
+        static void Plus(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+
+        static void Plus(double x, double y, out double z)
+        {
+            z = x + y;
+        }
+    }
+}
