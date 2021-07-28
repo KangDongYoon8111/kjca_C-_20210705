946b386dc298cdf649d08fb7f4da42e3cb839504
myooto <86944250+myooto@users.noreply.github.com>
2021-07-28 15:36:45 +0900

    Add files via upload

---
+++ 20210728/20210728_ex1_김도현.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _20210728_ex1_김도현
+{
+    class MainApp
+    {
+        static double Square(double arg)
+        {
+            return arg * arg;
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
---
+++ 20210728/20210728_ex2_김도현.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20210728_ex2_김도현
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, out mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+        public static void Mean(double a, double b, double c, double d, double e, out double mean)
+        { 
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
