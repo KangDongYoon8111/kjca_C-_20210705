aea40a41b2df51100077f3df745b3faa9cfb6e49
ohsunmin <87164935+ohsunmin@users.noreply.github.com>
2021-07-28 15:30:33 +0900

    Add files via upload

---
+++ 20210728/20210728_ex2_오선민.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace p216_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, ref mean);
+
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        static void Mean(double a, double b, double c, double d, double e, ref double mean)
+        {
+            mean = (a + b + c + d + e + mean) / 5;
+        }
+    }
+}
