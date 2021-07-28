5c53aa0c9373319dca5ba11555baaa8cc3e0dbfc
rt7356 <86944308+rt7356@users.noreply.github.com>
2021-07-28 15:41:49 +0900

    Add files via upload

---
+++ 20210728/20210728_ex_곽재우.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _20210728_ex1_곽재우.cs
+{
+    class Program
+    {
+        static double Square(double arg)
+        {
+            double i = arg * arg;
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        
+        }
+    }
+}
