bdaef71b408e5bc90ca056e7e51d6fc57e97c12e
mssung34 <86944313+mssung34@users.noreply.github.com>
2021-07-28 15:29:25 +0900

    Add files via upload

---
+++ 20210728/20210728_ex1_성민식.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20210728_ex1_성민식
+{
+    class Program
+    {
+        static double Square(double arg)
+        {
+            return arg * arg;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+            double arg = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
