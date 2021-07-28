71b0e0e1cb1bba6cb8367dd455133b6e6e0b3363
mylove9573 <86944269+mylove9573@users.noreply.github.com>
2021-07-28 15:30:54 +0900

    Add files via upload

---
+++ 20210728/20210728_ex1_표원식.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _20210728_ex1_표원식
+{
+    class Program
+    {
+        
+
+        
+        /*static double Square(double arg)
+        {
+
+            double i = 0;
+            i = arg * arg;
+            return i;
+
+        }
+
+        static int Square(int arg)
+        {
+            int i = 0;
+            i = arg * arg;
+            return i;
+        }
+
+
+        static void Main(string[] args)
+        {
+            bool a = true;
+            while (a) { 
+            Console.WriteLine("수를 입력하세요 :");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+            Console.WriteLine();
+            }
+        } */
+        
+    }
+}
