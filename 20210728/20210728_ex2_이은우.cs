bad54037c980535df24dfc265c3aa07192b5a339
이은우 <86944249+eunwool@users.noreply.github.com>
2021-07-28 15:35:03 +0900

    Add files via upload

---
+++ 20210728/20210728_ex2_이은우.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _20210728_ex2_이은우
+{
+    class Program
+    {
+        
+        public static void Main()
+        {
+            double mean = 0;
+
+            Mean(1, 2, 3, 4, 5, mean);
+            Console.WriteLine("평균: {0}", mean);
+
+
+        }
+        public static void Mean(
+            double a, double b, double c, double d, double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5; //
+            
+        }
+    }
+}
+// ref안씀
---
+++ 20210728/20210728_ex3_이은우.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _20210728_ex3_이은우
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+            Plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+            Plus(x, y, out resultB);
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, resultB);
+
+        }
+        public static void Plus(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+        public static void Plus(double x, double y, out double z)
+        {
+            z = x + y;
+
+        }
+    }
+}
