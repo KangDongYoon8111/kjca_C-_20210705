0ab3edb1862366cf8bbce05a4fc9b02ce87292ab
이은우 <86944249+eunwool@users.noreply.github.com>
2021-07-28 15:31:40 +0900

    Add files via upload

---
+++ 20210728/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _21210728_ex1_이은우
+{
+    class Program
+    {
+        static double Square(double arg)
+        {
+            return arg * arg;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("수를입력하시오: ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+        }
+    }
+}
